31f8e6e4cf5c7f5e09a3ac5ae736235efe67fd3f
Rahul kapoor <kapoorrahul304@gmail.com>
2023-07-01 14:30:29 +0530

    assign complete using Adding propTypes

---
+++ block-BRaaaq/my-app/src/components/App.js
@@ -0,0 +1,18 @@
+import Header from './Header';
+import Footer from './Footer';
+import Hero from "./Hero"
+import Articles from './Articles';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero/>
+      <Articles/>
+      <Footer />
+
+    </>
+  );
+}
+
+export default App;
---
+++ block-BRaaaq/my-app/src/components/Footer.js
@@ -0,0 +1,17 @@
+function Footer(){
+    return(
+        <>
+        <div className="center">
+        <div className="container ">
+
+            <p>&copy; AltCampus Template</p>
+
+        </div>
+
+        </div>
+        
+        </>
+    )
+}
+
+export default Footer;
