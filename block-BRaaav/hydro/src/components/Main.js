44ffde4fbfd0b22722b5529aa7d7f55d87046ffa
Rahul kapoor <kapoorrahul304@gmail.com>
2023-07-04 12:07:23 +0530

    Assign complete convert website into React

---
+++ block-BRaaav/hydro/src/components/Main.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import About from './About';
+import Blog from './Blog';
+import Work from './Work';
+import Contact from './Contact';
+import Footer from './Footer';
+function Main() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Blog />
+      <Work />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+export default Main;
